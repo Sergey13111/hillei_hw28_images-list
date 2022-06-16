2865ab40c1c9751a5ec20cd54cbec5ef8b671428
Sergey13111 <boiko.sergey93@gmail.com>
2022-06-16 15:52:03 +0300

    изминение начального состояние page и добавление кода HW-29

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -2,31 +2,29 @@ import axios from "../helpers/axios";
 import { useEffect, useState } from "react";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import { Container, Button, CircularProgress, Box } from '@mui/material';
-// import { Dialog, DialogTitle } from "@mui/material";
+import { Container, Button, CircularProgress, Box, Dialog, DialogTitle } from '@mui/material';
 
 const ImagesList = () => {
   const [images, setImages] = useState([]);
   const [isLoading, setLoading] = useState(true);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
 
-  // const [isDialogOpen, setDialogOpen] = useState(false);
-  // const [selectedImageData, setSelectedImageData] = useState(null);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [selectedImageData, setSelectedImageData] = useState({ author:null, width:null, height:null });
 
-  // const handleDialogOpen = ({...imageData}) => () => {
-  //   setDialogOpen(true);
-  //   setSelectedImageData({...imageData});
-  // };
-  // console.log(selectedImageData)
+  const handleDialogOpen = ({ author, width, height }) => () => {
+    setDialogOpen(true);
+    setSelectedImageData({ author, width, height });
+  };
  
-  // const handleDialogClose = () => {
-  //   setDialogOpen(false);
-  //   setSelectedImageData(null)
-  // };
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setSelectedImageData({ author:null, width:null, height:null });
+  };
 
   useEffect(() => {
     const params = {
-      page:`${page+1}`,
+      page:`${page}`,
       limit:10
     };
     
@@ -35,7 +33,6 @@ const ImagesList = () => {
     .then((newImages) => {
       setImages(prevImages => [...prevImages, ...newImages])
       setLoading(false);
-      // setLoadingB(false);
     })
     .catch((error) => {
       console.error(error);
@@ -43,23 +40,19 @@ const ImagesList = () => {
   },[page]);
 
     const handleClick = () => {
-    setPage(page=>page+=1 )
+    setPage(page => page += 1)
   };
 
   return (
     <>
-      {/* <Dialog open={isDialogOpen} onClose={handleDialogClose}>
-        <DialogTitle>Title</DialogTitle>
-        <p> </p>
-      </Dialog> */}
       <Container maxWidth="md" width="100%" margin="0 auto" sx={{textAlign:"center", padding:"20px"}}>
         <h1>Image Gallery</h1>
       
         <ImageList  cols={2} gap={10} rowHeight={194}>
-          {images.map(({ id, author, width, height, download_url }) => (
+          {images.map(({ author, width, height, id, download_url }) => (
             <ImageListItem key={id}>
               <img style={{ height: 100, objectFit: "contain" }} 
-                // onClick={handleDialogOpen({width,author,height})}
+                onClick={handleDialogOpen({ author, width, height })}
                 src={`${download_url}?w=144&h=194&fit=crop&auto=format`}
                 srcSet={`${download_url}?w=144&h=194&fit=crop&auto=format&dpr=2 2x`}
                 alt={author}
@@ -68,14 +61,30 @@ const ImagesList = () => {
             </ImageListItem>
           ))}
         </ImageList>
+
         {isLoading ? 
         <Box sx={{ display: "flex", justifyContent: "center" }}>
           <CircularProgress />
         </Box>:
         <Button color="primary" size="large" onClick={handleClick}>Show More</Button>}
       </Container>
+      
+      <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+        <DialogTitle sx={{ fontWeight:600, textAlign:"center" }}>Information</DialogTitle>
+        <Box p={1}>
+          <Box mb={1}>
+            Author: {selectedImageData.author}
+          </Box>
+          <Box  mb={1}>
+            Width: {selectedImageData.width}
+          </Box>
+          <Box  mb={1}>
+            Height: {selectedImageData.height}
+          </Box>
+        </Box>
+      </Dialog>
     </>
   );
-}
+};
 
 export default ImagesList;
